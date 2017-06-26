09b20c0eae4d5adcaef01813eee9cfb1603e7afa
Ryan Baseman <ryan.baseman@gmail.com>
2017-06-26 13:18:22 -0500

    Incremental changes

--- mind_prs/src/App.js
+++ mind_prs/src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+
+  handleClick() {    
+    var data = {
+       "Inputs": {
+                "input1":
+                [
+                    { 'time': "1", 'data': "1" },
+                    { 'time': "2", 'data': "3" },
+                    { 'time': "3", 'data': "2" },
+                    { 'time': "4", 'data': "1" },
+                ],
+        },
+      "GlobalParameters":  { }
+    };
+
+    $.ajax({
+      url: 'https://ussouthcentral.services.azureml.net/subscriptions/2dd028f929f044e0afa8f1feb12a5062/services/cf5785b34acd4e95be683c74d582c2da/execute?api-version=2.0&format=swagger',
+      type: 'POST',
+      dataType: 'json',
+      contentType: 'application/json',
+      cache: false,
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'bearer tdYLRk++vz3TQgwXu8r9muGnpz4y6GA2iTiQPtAgaekhOljDYDlDb+Y/PXdNlG44ZJetCSl/Gee39Z2dH+g9TQ==');
+      },
+      data: JSON.stringify(data),
+      success: function(data) {
+            this.setState({ data: data }); // Notice this
+          }.bind(this),
+      error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+          }.bind(this)
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
+          <button onClick={() => this.handleClick()}>Submit</button>
         </p>
       </div>
     );
-  }
+  }  
 }
 
 export default App;
