3c5cbc8a4b54b3404d04ba67157d4c295a243535
Ryan Baseman <ryan.baseman@gmail.com>
2017-07-04 20:04:25 -0500

    Trying out timeseries-analysis package

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-// import * as ml from 'machine-learning';
-var ml = require('machine-learning');
-// Linear Regression: y = 1000 + 200 * x
-const inputs = new ml.Matrix([[5], [7], [9], [11], [13]]);
-const targets = new ml.Matrix([[2000], [2400], [2800], [3200], [3600]]);
+var timeseries = require("timeseries-analysis");
+
+var data = [0,1,0,2,1,0,1,0,2,1,0,2,2,1,1,0,0,1,0,0,2,2];
  
-const linearRegression = new ml.LinearRegression();
-linearRegression.setNumberOfEpochs(10000);
-linearRegression.setLearningRate(0.02);
+// Load the data
+var t = new timeseries.main(timeseries.adapter.fromArray(data));
  
-linearRegression.train(inputs, targets);
-const predictions = linearRegression.predict(inputs);
-console.log(predictions.toArray());
+// We're going to forecast the 11th datapoint
+var forecastDatapoint	= 2;	
+ 
+// We calculate the AR coefficients of the dataset
+var coeffs = t.ARMaxEntropy({
+    data:	t.data
+});
+ 
+// Now, we calculate the forecasted value of the next datapoint using the AR coefficients:
+var forecast	= 0;	// Init the value at 0.
+for (var i=0;i<coeffs.length;i++) {	// Loop through the coefficients
+    forecast -= t.data[10-i][1]*coeffs[i];
+    // Explanation for that line:
+    // t.data contains the current dataset, which is in the format [ [date, value], [date,value], ... ]
+    // For each coefficient, we substract from "forecast" the value of the "N - x" datapoint's value, multiplicated by the coefficient, where N is the last known datapoint value, and x is the coefficient's index.
+}
+console.log("forecast",forecast);
+// Output: 92.7237232432106
