84d5d3df93fd127d02378adcf5d0e4e34c3adf67
Ryan Baseman <ryan.baseman@gmail.com>
2017-07-13 22:37:03 -0500

    Saving changes...

--- pi-view/src/components/rps/rps.jsx
+++ pi-view/src/components/rps/rps.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 
 class Home extends Component {
+    init() {
+        var socket = io('http://localhost:3001');
+        socket.on('chat message', function(msg) {
+            window.console.log(msg);
+        });
+    }
     render() {
         return (
             <div className="jumbotron">
