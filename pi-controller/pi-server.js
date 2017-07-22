1180fe11a5999cea3b8b095588c1a03cbee0cffb
Ryan Baseman <ryan.baseman@gmail.com>
2017-07-21 20:20:42 -0500

    Trying xbox controller

--- pi-controller/pi-server.js
+++ pi-controller/pi-server.js
@@ -1,18 +1,63 @@
 var cylon = require('cylon');
 
+// cylon.robot({
+//   connections: {
+//     raspi: { adaptor: 'raspi' }
+//   },
+
+//   devices: {
+//     button: { driver: 'button', pin: 11 }
+//   },
+
+//   work: function(my) {
+//      my.button.on('push', function() {
+//       console.log("Button pushed!");
+//     });
+//   }
+// }).start();
+
 cylon.robot({
   connections: {
-    raspi: { adaptor: 'raspi' }
+    joystick: { adaptor: 'joystick' }
   },
 
   devices: {
-    led: { driver: 'led', pin: 11 },
-    button: { driver: 'button', pin: 2 }
+    controller: { driver: 'xbox-360' }
   },
 
   work: function(my) {
-     my.button.on('push', function() {
-      console.log("Button pushed!");
+     ["a", "b", "x", "y"].forEach(function(button) {
+      my.controller.on(button + ":press", function() {
+        console.log("Button " + button + " pressed.");
+      });
+
+      my.controller.on(button + ":release", function() {
+        console.log("Button " + button + " released.");
+      });
+    });
+    
+    my.controller.on("left_x:move", function(pos) {
+      console.log("Left Stick - X:", pos);
+    });
+
+    my.controller.on("left_y:move", function(pos) {
+      console.log("Left Stick - Y:", pos);
+    });
+
+    my.controller.on("right_x:move", function(pos) {
+      console.log("Right Stick - X:", pos);
+    });
+
+    my.controller.on("right_y:move", function(pos) {
+      console.log("Right Stick - Y:", pos);
+    });
+
+    my.controller.on("lt:move", function(pos) {
+      console.log("Left Trigger: ", pos);
+    });
+
+    my.controller.on("rt:move", function(pos) {
+      console.log("Right Trigger: ", pos);
     });
   }
 }).start();
