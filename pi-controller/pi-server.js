5189ed5335b5d084883ff604bad0467dc225229d
Ryan Baseman <ryan.baseman@gmail.com>
2017-07-21 20:04:47 -0500

    Trying a button

--- pi-controller/pi-server.js
+++ pi-controller/pi-server.js
@@ -6,10 +6,13 @@ cylon.robot({
   },
 
   devices: {
-    led: { driver: 'led', pin: 11 }
+    led: { driver: 'led', pin: 11 },
+    button: { driver: 'button', pin: 2 }
   },
 
   work: function(my) {
-    every((1).second(), my.led.toggle);
+     my.button.on('push', function() {
+      console.log("Button pushed!");
+    });
   }
 }).start();
